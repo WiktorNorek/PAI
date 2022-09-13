dbcb9dd33b853d97af490a301b77bcfe7bea037a
Wiktor Norko (Uczeń) <wnorko155@technikumkreatywne.pl>
2022-09-13 11:47:47 +0200

    Php 1

---
+++ php.php
@@ -0,0 +1,12 @@
+﻿<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />  
+		<title>Test</title>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+		<?php
+		$zmi="Zmienna";
+		echo "Hello World <br> Hello World <br> Hello World <br>", $zmi ; 
+		?>
+	</body>
