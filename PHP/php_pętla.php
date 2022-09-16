5b7742ffdca4584c640afcb438baa211e0ca023b
Wiktor Norko (Uczeń) <wnorko155@technikumkreatywne.pl>
2022-09-16 13:00:14 +0200

    php-petle

---
+++ PHP/php_pętla.php
@@ -0,0 +1,18 @@
+﻿<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />  
+		<title>Test</title>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+		<?php
+		for($count = 1; $count <= 10; ++$count)
+		{
+			for($licz = 1; $licz <= 10; ++$licz){
+			$wyn = $count*$licz;
+			echo $count, "*", $licz, " = ", $wyn, "<br>";
+			}
+			echo "<br>";
+		}
+		?>
+	</body>
---
+++ PHP/php_pętla2.php
@@ -0,0 +1,25 @@
+﻿<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />  
+		<title>Test</title>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+		<?php
+		for($count = 1; $count <= 6; ++$count)
+		{
+			for($licz = 1; $licz <= 5; ++$licz){
+				if ($licz==1 || $licz==5){
+					echo "*";
+					}
+				elseif ($count==1 || $counts==6){
+					echo "*";
+					}
+				else {
+					echo "#";
+					}
+		}
+		echo "<br>";
+		}
+		?>
+	</body>
