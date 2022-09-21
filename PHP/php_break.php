e502045da402270e8aece3b6b434c72652246477
Wiktor Norko (Uczeń) <wnorko155@technikumkreatywne.pl>
2022-09-21 14:45:51 +0200

    petle2

---
+++ PHP/php_break.php
@@ -0,0 +1,14 @@
+﻿<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />  
+		<title>Test</title>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+		<?php
+		for($count = -11; $count <= 11; ++$count){
+		if($count % 5 == 0) continue;
+		echo $count, " <br>";}
+		
+		?>
+	</body>
