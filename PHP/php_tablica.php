70554cbac31fb3ea30c7f29d1968f019fefbe567
Wiktor Norko (Uczeń) <wnorko155@technikumkreatywne.pl>
2022-09-27 12:54:44 +0200

    Komentarz

---
+++ PHP/php_tablica.php
@@ -0,0 +1,27 @@
+﻿<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />  
+		<title>Test</title>
+		<link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+<?php
+    /* $tytul[] = "1984";
+    $tytul[] = "Nowy, wspaniały świat";
+    $tytul[] = "Don Kichot z La Manchy";
+
+    for($j=2; $j>=0; $j--){
+        echo "$j: $tytul[$j] <br>";
+    } */
+	
+	$właść = array('tekst' => "Tak",
+	'teksto' => "Nie",
+	'text' => "Może");
+	$j = 0;
+	
+	foreach($właść as $item){
+        echo "$j: $item <br>";
+        ++$j;
+    }
+?>
+	</body>
