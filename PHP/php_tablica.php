fe301e025a12cfc1c61c23c1abde4d1d8ae8adc7
Wiktor Norko (Uczeń) <wnorko155@technikumkreatywne.pl>
2022-09-28 14:35:38 +0200

    Tablicev2

--- PHP/php_tablica.php
+++ PHP/php_tablica.php
@@ -14,7 +14,7 @@
         echo "$j: $tytul[$j] <br>";
     } */
 	
-	$właść = array('tekst' => "Tak",
+	/* $właść = array('tekst' => "Tak",
 	'teksto' => "Nie",
 	'text' => "Może");
 	$j = 0;
@@ -22,6 +22,61 @@
 	foreach($właść as $item){
         echo "$j: $item <br>";
         ++$j;
+    }*/
+	
+	 /*$products = array(
+                'paper' => array(
+                    'copier' => "Do kserokopiarek i uniwersalny",
+                    'inkjet' => "Do drukarek atramentowych",
+                    'laser' => "Do drukarek laserowych",
+                    'photo' => "Papier fotograficzny"),
+                'pens' => array(
+                    'ball' => "Długopisy",
+                    'hilite' => "Markery przezroczyste",
+                    'marker' => "Markery zwykłe"),
+                'misc' => array(
+                    'tape' => "Taśmy klejące",
+                    'glue' => "Kleje",
+                    'clips' => "Spinacze")
+                );
+            echo "<pre>";
+        
+            echo $products['paper']['copier']," <br>";
+			echo $products['pens']['ball']," <br>";
+			echo $products['misc']['tape']," <br>";
+			
+			    foreach($products as $section => $items)
+                foreach($items as $key => $value)
+					if ($key == 'copier' OR $key == 'ball' OR $key == 'tape')
+                    echo "$section: \t $key \t ($value)<br>";
+            echo "</pre>";  */
+			
+			
+			
+			
+			
+			
+		
+			    $chessboard = array(
+        array('w','s','g','h','k','g','s','w'),
+        array('p','p','p','p','p','p','p','p'),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array('P','P','P','P','P','P','P','P'),
+        array('W','S','G','H','K','G','S','W'),
+    );
+    $chessboard[6][4] = ' ';
+	$chessboard[4][4] = 'P';
+
+    echo "<pre>";
+    foreach($chessboard as $row)
+    {
+        foreach($row as $piece)
+            echo "$piece ";
+        echo "<br>";
     }
+    echo "</pre>";
 ?>
 	</body>
