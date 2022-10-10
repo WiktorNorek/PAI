38a7a48d0a6b5a6fc57e4f73950e46d7dc6fd6a3
Wiktor Norko (Uczeń) <wnorko155@technikumkreatywne.pl>
2022-10-10 13:05:39 +0200

    Funkcje

---
+++ PHP/funkcje.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+    <head>
+        <title>PAI</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+	<form action="./funkcje.php" method="POST">
+	Podaj swój wiek<input type="number" name="wiek" />
+	<input type="submit" />
+	</form>
+		
+		
+        <?php
+		
+			function funkcja($inna){
+			if($inna >= 0 && $inna <= 18){
+			echo "Jesteś młody";}
+			elseif($inna >= 19 && $inna <= 60){
+				echo "Jesteś dorosły";}
+			else{
+			echo "Jesteś stary";}}
+			
+			
+			if(isset($_POST['wiek'])){
+				$inna = $_POST['wiek'];
+				funkcja($inna);}
+        ?>
+    </body>
+</html>
