530c26c3dfd7cab5f3a6c4e1676e56b31a96ed45
Wiktor Norko (Uczeń) <wnorko155@technikumkreatywne.pl>
2022-10-11 11:00:36 +0200

    pamięć

---
+++ PHP/funkcje — kopia.php
@@ -0,0 +1,9 @@
+﻿<!DOCTYPE html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	
+	<body>
+		<form action="./funkcje2.php" method="POST">
+		<input style="text"
+	</body>
---
+++ PHP/funkcje2.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	
+	<body>
+		<form action="./funkcje2.php" method="POST">
+			<input type="text" value="Imie" name="Imie"><br><br>
+			<input type="text" value="Nazwisko" name="Nazwisko"><br><br>
+			M <input type="radio" name="plec" value="m"><br>
+			K <input type="radio" name="plec" value="k"><br><br>			
+			<input type="submit" value="Wyślij"><br><br>
+		</form>
+		
+		<?php
+		
+		if(isset($_POST['Imie'])){
+				$Imi = $_POST['Imie'];}
+		
+		if(isset($_POST['Nazwisko'])){
+				$Naz = $_POST['Nazwisko'];}
+		
+		
+		if(isset($_POST['plec'])){
+                if($_POST['plec'] == 'm') echo "Witam Pana ",$Imi," ",$Naz, "<br>";
+		elseif($_POST['plec'] == 'k') echo "Witam Panią ",$Imi," ",$Naz, "<br>";}
+		?>
+	</body>
