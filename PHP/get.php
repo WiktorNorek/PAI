80df4900ef932e1c671c04648e4591aac4be52b8
WiktorNorek <101796208+WiktorNorek@users.noreply.github.com>
2022-10-25 14:48:31 +0200

    Add files via upload

---
+++ PHP/get.php
@@ -0,0 +1,29 @@
+﻿<?php 
+
+if(isset($_GET["page"]))
+	$strona = $_GET["page"];
+else
+	$strona = null;
+
+echo "strona: ".$strona."<br>";
+
+
+?>
+<html>
+	<body>
+	<a href="dolaczanie.php?page=main"> Strona 1 </a> <br>
+	<a href="dolaczanie.php?page=news"> Strona 2 </a>
+		<header> 
+			jakas zawartosc headera
+		</header>
+		<div>
+		
+			
+			<?php if(($strona)!=null) include($strona.".php"); ?>
+			
+		</div>
+		<footer> 
+			jakas zawartosc footera
+		</footer>
+	</body>
+</html>
